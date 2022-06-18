33f1ca8a544e1e1fd5518eb51a37fb69d9055de3
RICO <ricoyou99@gmail.com>
2022-06-18 21:24:06 +0700

    fix new tab

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -12,33 +12,30 @@ export default function Footer() {
       <div className='max-w-screen-xl px-4 py-6 mx-auto sm:px-6 lg:px-8'>
         <div className='max-w-3xl mx-auto space-y-6'>
           <div className='flex justify-center space-x-6'>
-            <Link
-              href={'https://github.com/Alien-404'}
-              target={'_blank'}
-              rel={'noopener noreferrer'}
+            <a
+              className='text-slate-200 hover:text-opacity-75'
+              href='https://github.com/Alien-404'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <a className='text-slate-200 hover:text-opacity-75'>
-                <AiOutlineGithub className='w-8 h-8' />
-              </a>
-            </Link>
-            <Link
-              href={'https://www.instagram.com/stfu_404/'}
-              target={'_blank'}
-              rel={'noopener noreferrer'}
+              <AiOutlineGithub className='w-8 h-8' />
+            </a>
+            <a
+              className='text-slate-200 hover:text-opacity-75'
+              href='https://www.instagram.com/stfu_404/'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <a className='text-slate-200 hover:text-opacity-75'>
-                <AiOutlineInstagram className='w-8 h-8' />
-              </a>
-            </Link>
-            <Link
-              href={'https://www.linkedin.com/in/rico-w-6aa984179/'}
-              target={'_blank'}
-              rel={'noopener noreferrer'}
+              <AiOutlineInstagram className='w-8 h-8' />
+            </a>
+            <a
+              className='text-slate-200 hover:text-opacity-75'
+              href='https://www.linkedin.com/in/rico-w-6aa984179/'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <a className='text-slate-200 hover:text-opacity-75'>
-                <AiOutlineLinkedin className='w-8 h-8' />
-              </a>
-            </Link>
+              <AiOutlineLinkedin className='w-8 h-8' />
+            </a>
           </div>
           <nav className='relative flex flex-wrap justify-center gap-8 p-4 text-sm font-bold border-2 border-slate-200 rounded-md'>
             <Link
