a9c16c39caafe19f880a79c4447551f7f7501f26
RICO <ricoyou99@gmail.com>
2022-07-08 17:52:59 +0700

    add google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 import { TerminalContextProvider } from 'react-terminal';
 
 function MyApp({ Component, pageProps }) {
   return (
     <TerminalContextProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}
+        strategy='lazyOnload'
+      />
+      <Script strategy='lazyOnload' id='google-analytics'>
+        {` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.GTAG_ID});`}
+      </Script>
       <Component {...pageProps} />
     </TerminalContextProvider>
   );
