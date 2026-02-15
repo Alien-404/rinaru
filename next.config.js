1da1a6e4eb3cb2e12f32014cf276c7c65cbd3812
rico <rico@smartchaincode.com>
2026-02-15 09:31:39 +0700

    fix: image load

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['media.graphassets.com'],
+    domains: ['media.graphassets.com', 'ap-northeast-1.graphassets.com'],
   },
 };
 
